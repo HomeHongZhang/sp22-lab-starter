9be27e6fcce94803623101578c3cabba6eca3bd7
Hong Zhang <hong.z@rioslab.org>
2022-06-29 20:17:52 +0800

    [ADD] finish Excercise 1-4

--- lab03/ex3.s
+++ lab03/ex3.s
@@ -23,43 +23,43 @@ dest:
 
 .text
 fun:
-    addi t0, a0, 1
-    sub t1, x0, a0
-    mul a0, t0, t1
-    jr ra
+    addi t0, a0, 1 		# (x+1)
+    sub t1, x0, a0		# (-x)
+    mul a0, t0, t1		# -x * (x+1)
+    jr ra				# return with the result
 
 main:
     # BEGIN PROLOGUE
-    addi sp, sp, -20
-    sw s0, 0(sp)
-    sw s1, 4(sp)
-    sw s2, 8(sp)
-    sw s3, 12(sp)
+    addi sp, sp, -20	# allocate stack with 5 frames
+    sw s0, 0(sp)		# sum
+    sw s1, 4(sp)		# source
+    sw s2, 8(sp)		# dest
+    sw s3, 12(sp)		# offset of index
     sw ra, 16(sp)
     # END PROLOGUE
-    addi t0, x0, 0
-    addi s0, x0, 0
-    la s1, source
-    la s2, dest
+    addi t0, x0, 0		# k = 0
+    addi s0, x0, 0		# sum = 0
+    la s1, source 		# load the address of the source
+    la s2, dest   		# load the address of the destination
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
-    sw t0, 0(sp)
-    sw t2, 4(sp)
-    jal fun
-    lw t0, 0(sp)
-    lw t2, 4(sp)
-    addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
-    jal x0, loop
+    slli s3, t0, 2		# get begin address offset
+    add t1, s1, s3		# t1 = source[k]
+    lw t2, 0(t1)		# t2 = source[k], write to memory
+    beq t2, x0, exit	# exit loop once 
+    add a0, x0, t2		# a0 = source[k]
+    addi sp, sp, -8		# allocate 2 new frames
+    sw t0, 0(sp)		# save k
+    sw t2, 4(sp)		# save source[k]
+    jal fun				# call `fun()`
+    lw t0, 0(sp)		# restore k
+    lw t2, 4(sp)		# restore source[k]
+    addi sp, sp, 8		# deallocate 2 frames
+    add t2, x0, a0		# source[k] = a0
+    add t3, s2, s3		# t3 = dest[k]
+    sw t2, 0(t3)		# t2 = dest[k], write to memory
+    add s0, s0, t2		# sum += dest[k]
+    addi t0, t0, 1		# k++
+    jal x0, loop		# jump to `loop` without saving the return address
 exit:
     add a0, x0, s0
     # BEGIN EPILOGUE
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,12 +1,12 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 30
 
 .text
 main:
-    la t0, n
-    lw a0, 0(t0)
+    la t0, n				# load the address of value of n
+    lw a0, 0(t0)			# a0 = n, serve as parameter
     jal ra, factorial
 
     addi a1, a0, 0
@@ -22,3 +22,23 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi sp, sp, -8		
+    sw s0, 0(sp)		# save s0
+    sw ra, 4(sp)		# save ra
+    
+    addi s0, x0, 1		# res = 1
+    addi t1, x0, 1		# i = 1
+    add t2, x0, a0		# n
+loop:
+    beq t1, t2, finish  
+    addi t1, t1, 1		# i += 1
+    mul s0, s0, t1    	# res *= i
+    jal x0, loop
+	
+finish:
+	add a0, x0, s0	# save return value
+    
+    lw s0, 0(sp)	# restore s0
+    lw ra, 4(sp)	# restore ra
+    
+    jr ra
